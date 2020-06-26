e99602552723717a65ed05b47b3af92a4b4edce4
Anagha Sivadas <42840450+angie1015@users.noreply.github.com>
2020-06-26 09:08:46 +0530

    Create deleteNode.cpp

---
+++ code-examples/linked-lists/deleteNode.cpp
@@ -0,0 +1,17 @@
+/**
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/553
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val;
+        node->next=node->next->next;
+        
+    }
+};
