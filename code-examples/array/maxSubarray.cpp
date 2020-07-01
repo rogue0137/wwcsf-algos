14e4db5d21f466daae3cce660e5feadabc100be1
Anagha Sivadas <42840450+angie1015@users.noreply.github.com>
2020-07-01 13:49:39 +0530

    Update maxSubarray.cpp

--- code-examples/array/maxSubarray.cpp
+++ code-examples/array/maxSubarray.cpp
@@ -4,9 +4,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         int curSum=0, maxSum= nums[0], i, n=nums.size();
         for(i=0; i<n; i++){
-            if(curSum<0)
-                curSum=0;
-            curSum+=nums[i];
+            curSum=max(nums[i], curSum+nums[i]);
             maxSum= max(curSum, maxSum);
         }
         return maxSum;
