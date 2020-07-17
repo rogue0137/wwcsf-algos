3226e5d87af1c4aff1e4751a3d03392c2f84c5df
Anagha Sivadas <42840450+angie1015@users.noreply.github.com>
2020-07-17 19:11:20 +0530

    Update boats.cpp

--- code-examples/array/boats.cpp
+++ code-examples/array/boats.cpp
@@ -1,25 +1,5 @@
-/* 
-https://leetcode.com/problems/boats-to-save-people/
-
-Read this code to understand it better
-class Solution {
-public:
-    int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(), people.end());
-        int i=0, j=people.size()-1, count=0;
-        while(i<j){
-            if(people[j] + people[i] <= limit){
-                count++;
-                i++;
-            }
-            else
-                count++;
-            j--;
-        }
-        return count;
-    }
-};
-*/
+ 
+// https://leetcode.com/problems/boats-to-save-people/
 
 class Solution {
 public:
