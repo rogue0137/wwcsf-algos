45b4cd4e30427ad47e5d691f60d3c9b36f08cd03
Gabriela Martinez <gmartinezsan@gmail.com>
2020-07-18 21:49:12 -0500

    Adding several charp code examples for dfs, matrix, and sorting

---
+++ code-examples/depth-first-search/csharp/surrounded_regions/Program.cs
@@ -0,0 +1,158 @@
+﻿/* 
+130. Surrounded Regions
+Medium
+
+Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+Leetcode link 
+https://leetcode.com/problems/surrounded-regions/
+
+Solution
+
+1. Traverse the matrix and change all the 'O' by '-'
+2. Check the edges and it's adjacent cell's to be 'O' as originally
+3. The rest of the '-' are the cells that can be flipped to 'X'
+
+*/
+
+using System;
+
+namespace surrounded_regions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matrix = new int[5,4];
+            Console.WriteLine(matrix.GetLength(0) + " " + matrix.GetLength(1));
+            
+
+            return;
+
+            // board 6 x 6
+            char[][] board =
+                new char[6][]
+                {
+                    new char[6] { 'X', 'O', 'X', 'X', 'X', 'X' },
+                    new char[6] { 'X', 'O', 'X', 'X', 'O', 'X' },
+                    new char[6] { 'X', 'X', 'X', 'O', 'O', 'X' },
+                    new char[6] { 'O', 'X', 'X', 'X', 'X', 'X' },
+                    new char[6] { 'X', 'X', 'X', 'O', 'X', 'O' },
+                    new char[6] { 'O', 'O', 'X', 'O', 'O', 'O' }
+                };
+
+            Console.WriteLine("======= input =====");
+            PrintBoard (board);
+
+            ReplaceSurrounded (board);
+
+            PrintBoard (board);
+
+            Console.WriteLine("======= output =====");
+
+             PrintBoard (board);
+        }
+
+        public static void ReplaceSurrounded(char[][] board)
+        {
+            // Step 1 - Replace all O by -
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[0].Length; j++)
+                {
+                    if (board[i][j] == 'O')
+                    {
+                        board[i][j] = '-';
+                    }
+                }
+            }
+
+            // Step 2 - check all edges
+            // top side
+            for (int i = 0; i < board[0].Length; i++)
+            {
+                if (board[0][i] == '-') DFSHelper(board, 0, i, '-', 'O');
+            }
+            // bottom side
+            for (int i = 0; i < board[0].Length; i++)
+            {
+                if (board[board.Length - 1][i] == '-') DFSHelper(board, board.Length - 1, i, '-', 'O');
+            }
+
+            //left side
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (board[i][0] == '-') DFSHelper(board, i, 0, '-', 'O');
+            }
+
+            //right side
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (board[i][board[0].Length - 1] == '-') DFSHelper(board, i, board[0].Length - 1, '-', 'O');
+            }
+         
+            // step 3 replace the remaining '-' by 'X
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[0].Length; j++)
+                {
+                    if (board[i][j] == '-')
+                    {
+                        board[i][j] = 'X';
+                    }
+                }
+            }
+
+        }
+
+        private static void DFSHelper(
+            char[][] board,
+            int row,
+            int col,
+            char prevChar,
+            char newChar
+        )
+        {
+            // base case
+            if (
+                row < 0 ||
+                row >= board.Length ||
+                col < 0 ||
+                col >= board[0].Length
+            ) return;
+
+
+            if (board[row][col] != prevChar)
+              return;
+              
+            board[row][col] = newChar;
+
+            //north
+            DFSHelper(board, row - 1, col, prevChar, newChar);
+
+            //south
+            DFSHelper(board, row + 1, col, prevChar, newChar);
+
+            //east
+            DFSHelper(board, row, col - 1, prevChar, newChar);
+
+            //west
+            DFSHelper(board, row, col + 1, prevChar, newChar);
+        }
+
+        public static void PrintBoard(char[][] board)
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                var chars = board[i];
+                Console.Write("| ");
+                foreach (char c in chars) Console.Write(" " + c);
+                Console.WriteLine(" |");
+            }
+            Console.WriteLine("_____________");
+        }
+    }
+}
---
+++ code-examples/two-dimensional-matrix/csharp/spiralmatriz/Program.cs
@@ -0,0 +1,145 @@
+﻿/*
+54. Spiral Matrix
+Medium
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+Leetcode link https://leetcode.com/problems/spiral-matrix/
+The following code has two implementations: SpiralMatrixToArray and SpiralMatrixToConsole
+*/
+
+using System;
+
+namespace spiralmatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test 1
+            // int[,] a = new int [,] {{2,4,6,8}, {5,9,12,16}, {2,11,5,9}, {3,2,1,8}};
+            
+            // Test 2
+            int[,] a = new int[,] { { 1 }, { 2 } };
+            var result = SpiralMatrixToArray(a);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+
+        static int[] SpiralMatrixToArray(int[,] inputMatrix)
+        {
+            if (inputMatrix.GetLength(1) == 0 && inputMatrix.GetLength(0) == 0)
+                return new int[0];
+
+            var iterator = 0;
+            int row = 0;
+            int colright = inputMatrix.GetLength(1) - 1;
+            int colLeft = 0;
+            var l = inputMatrix.GetLength(1) * inputMatrix.GetLength(0);
+            var spiralCopy = new int[l];
+            var indexSpiral = 0;
+            var bottom = inputMatrix.GetLength(0) - 1; // number of rows
+
+            while (row < bottom && colLeft < colright)
+            {
+                if (iterator == 0)
+                {
+                    // top left to top right
+                    for (int i = colLeft; i <= colright; i++)
+                    {
+                        spiralCopy[indexSpiral++] = inputMatrix[row, i];
+                    }
+                    row++;
+                    iterator = 1;
+                }
+
+                if (iterator == 1)
+                {
+                    // right top to the bottom right
+                    for (int i = row; i <= bottom; i++)
+                    {
+                        spiralCopy[indexSpiral++] = inputMatrix[i, colright];
+                    }
+                    colright--;
+                    iterator = 2;
+                }
+
+                if (iterator == 2)
+                {
+                    // bottom right to bottom left
+                    for (int i = colright; i >= colLeft; i--)
+                    {
+                        spiralCopy[indexSpiral++] = inputMatrix[bottom, i];
+                    }
+                    bottom--;
+                    iterator = 3;
+                }
+
+                if (iterator == 3)
+                {
+                    // bottom left to top right
+                    for (int i = bottom; i >= row; i--)
+                    {
+                        spiralCopy[indexSpiral++] = inputMatrix[i, colLeft];
+                    }
+                    colLeft++;
+                    iterator = 0;
+                }
+            }
+            return spiralCopy;
+        }
+
+        static void SpiralMatrixToConsole(int[,] a)
+        {
+            int rows = a.GetLength(0);
+            int columns = a.GetLength(1);
+
+            int top = 0;
+            int bottom = rows - 1;
+            int left = 0;
+            int right = columns - 1;
+            int direction = 0; // 0 left to righ, 1 top to bottom, 2 right to left, 3 bottom to top
+
+            while (bottom >= top && right >= left)
+            {
+                if (direction == 0)
+                {
+                    for (int i = left; i <= right; i++)
+                    {
+                        Console.Write(a[top, i]);
+                    }
+                    top++;
+                    direction = 1;
+                }
+                if (direction == 1)
+                {
+                    for (int i = top; i <= bottom; i++)
+                    {
+                        Console.Write(a[i, right]);
+                    }
+                    right--;
+                    direction = 2;
+                }
+                if (direction == 2)
+                {
+                    for (int i = right; i >= left; i--)
+                    {
+                        Console.Write(a[bottom, i]);
+                    }
+                    bottom--;
+                    direction = 3;
+                }
+                if (direction == 3)
+                {
+                    for (int i = bottom; i >= top; i--)
+                    {
+                        Console.Write(a[i, left]);
+                    }
+                    left++;
+                    direction = 0;
+                }
+            }
+        }
+    }
+}
